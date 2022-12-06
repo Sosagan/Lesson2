893e7cf41ac8f20b160ea7c60d042bbca1cd752d
Sosagan <119598373+Sosagan@users.noreply.github.com>
2022-12-06 15:36:54 +0300

    Add files via upload

---
+++ 1 номер.py
@@ -0,0 +1,5 @@
+fNum = int(input("Input first number: "))
+sNum = int(input("Input second number: "))
+print(fNum + sNum, fNum - sNum, 
+fNum * sNum,  fNum / sNum, 
+fNum ** sNum, fNum % sNum, fNum // sNum)2
---
+++ 4 номер.py
@@ -0,0 +1,3 @@
+import math
+rad = float(input("Input radius, please: "))
+print(math.pi * (rad ** 2))
